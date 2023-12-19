96a866b63f8439ffea9ce07cdd28947be1c098c8
Bloodray636 <d.korolev2003666@bk.ru>
2023-12-19 19:46:28 +0600

    Childrens art school

---
+++ assets/script/script.js
@@ -0,0 +1,59 @@
+$("#owl-demo-1").owlCarousel({
+  loop:true,
+  nav: true,
+  margin:10,
+  responsiveClass:true,
+  mouseDrag: true,
+  touchDrag: true,
+  pullDrag: true,
+  autoWidth: false,
+  autoplay: true,
+  autoplayHoverPause: true,
+  autoHeight:true,
+  dots: true,
+  dotsEach: true,
+  responsive:{
+      0:{
+        items:1,
+        nav:true
+      },
+      575:{
+          items:2,
+          nav:true
+      },
+      991:{
+          items:4,
+          nav:true
+      }
+  }
+});
+
+$("#owl-demo-2").owlCarousel({
+  loop:true,
+  nav: true,
+  margin:10,
+  responsiveClass:true,
+  mouseDrag: true,
+  touchDrag: true,
+  pullDrag: true,
+  autoWidth: false,
+  autoplay: true,
+  autoplayHoverPause: true,
+  autoHeight:true,
+  dots: true,
+  dotsEach: true,
+  responsive:{
+    0:{
+      items:1,
+      nav:true
+    },
+    575:{
+        items:1,
+        nav:true
+    },
+    991:{
+        items:1,
+        nav:true
+    }
+  }
+});
